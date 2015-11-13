09e849a883c879ae172f9cebd179387540b2b825
MotionIsLife <woron186@mail.ru>
2015-11-13 07:55:54 +0300

    Кнопка поиска

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,87 @@
+
 /** @jsx React.DOM */
-'use strict';
-var HelloWorld = React.createClass({displayName: 'HelloWorld',
-	getInitialState: function() {
-		return {message: this.props.initialMessage};
-	},
-	clicked: function(event) {
-		this.setState({message: "Clicked to h3"});
-	},
-	render: function() {
-		return (
-			React.DOM.h3( {onClick:this.clicked}, this.state.message)
-		);
-	}
+
+var Img = React.createClass({
+  displayName: 'Img',
+
+  render: function() {
+    return (
+      React.DOM.img({src: "https://imagecomics.com/images/made/uploads/releases/AstronautsInTrouble_06-1_266_400_s_c1.png"})
+    );
+  }
 });
 
 React.renderComponent(
-	HelloWorld({initialMessage: 'Hello, Kitty!'}),
-	document.getElementById('app')
+  Img(null),
+  document.getElementById('app')
 );
+
+
+var ss = "Test";
+
+var HelloWorld = React.createClass({
+  displayName: 'HelloWorld',
+ 
+
+  getInitialState: function() {
+  iss : false;
+  isText = Boolean(true);
+    return {message: {ss}};
+  },
+
+
+  goodbye: function(event) {
+    this.setState({message: isText? 'Goodbye World.' : "Hello World"});
+	isText = !isText;
+  },
+
+
+  render: function() {
+    return (
+      React.DOM.h1({onClick:this.goodbye},
+          this.state.message)
+    );
+  }
+});
+
+React.renderComponent(
+  HelloWorld(null),
+  document.getElementById('app2')
+);
+
+
+var Btn = React.createClass({
+  displayName: 'Btn',
+  
+  toggleLiked: function() {
+	ss : "ddf";
+    this.setState({
+      liked: !this.state.liked
+    });
+  },
+  
+  getInitialState: function() {
+    return {
+      liked: false
+    }
+  },
+  
+  setText: function(){
+	console.info('Message');
+  },
+
+  render: function() {
+   var buttonClass = this.state.liked ? ' active' : '';
+    return (
+      React.DOM.button({onClick:this.toggleLiked}, caption='Btn', className={buttonClass})
+    );
+  }
+});
+
+React.renderComponent(
+  Btn(null),
+  document.getElementById('')
+);
+
+
+module.export = Btn2;
